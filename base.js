337714bde92f11e3e26b2799b77245d73b38d420
Jashua R. White <jashuawhite@gmail.com>
2018-03-04 23:54:12 -0500

    Add filter and link list items to map markers

--- base.js
+++ base.js
@@ -1,45 +1,126 @@
 function initMap() {
 
-  var contentString = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-    '<div id="bodyContent">'+
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the '+
-    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-    'south west of the nearest large town, Alice Springs; 450&#160;km '+
-    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    'Aboriginal people of the area. It has many springs, waterholes, '+
-    'rock caves and ancient paintings. Uluru is listed as a World '+
-    'Heritage Site.</p>'+
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-    '(last visited June 22, 2009).</p>'+
-    '</div>'+
-    '</div>';
-
   /* Map */
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: { lat: 39.954365, lng: -75.1608042 }
+    zoom: 10,
+    center: { lat: 40.05, lng: -75.09 },
+    mapTypeControl: false,
+    fullscreenControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_LEFT
+    }
   });
 
   /* Locations Data */
 
   var locations = [
     {
-      name: 'Location One',
-      address: '2918 Poplar Street #3 Philadelphia, PA 19130',
-      position: { lat: 39.954365, lng: -75.1608042 }
+    	"type": "hospital",
+    	"region": "montgomery",
+    	"name": "Abington Hospital",
+    	"address": "1200 Old York Rd.<br>Abington, PA 19001",
+    	"phone": "Phone: 215-481-2000",
+    	"url": "http://www.abingtonhealth.org/find-a-location/abington-memorial-hospital/",
+    	"position": { "lat": 40.119136, "lng": -75.119953 }
     },
     {
-      name: 'Location Two',
-      address: '833 Chestnut Street Philadelphia, PA 19107',
-      position: { lat: 39.9819479, lng: -75.2131994 }
+    	"type": "hospital",
+    	"region": "montgomery",
+    	"name": "Abington &ndash; Lansdale Hospital",
+    	"address": "100 Medical Campus Dr.<br>Lansdale, PA 19446",
+    	"phone": "Phone: 215-368-2100",
+    	"url": "http://www.abingtonhealth.org/find-a-location/lansdale-hospital/",
+    	"position": { "lat": 40.25115, "lng": -75.271454 }
+    },
+    {
+    	"type": "hospital",
+    	"region": "philadelphia",
+    	"name": "Thomas Jefferson University Hospital",
+    	"address": "111 South 11th Street<br>Philadelphia, PA 19107",
+    	"phone": "Main Phone: 215-955-6000<br>Appointment Phone: 1-800-JEFF-NOW (215-533-3669)",
+    	"url": "//hospitals.jefferson.edu/content/health/find-a-location/locations/tjuh-111-south-11th-street.html",
+    	"position": { "lat": 39.9498262, "lng": -75.1584227 }
+    },
+    {
+    	"type": "hospital",
+    	"region": "philadelphia",
+    	"name": "Jefferson Hospital for Neuroscience",
+    	"address": "900 Walnut Street<br>Philadelphia, PA 19107",
+    	"phone": "Main Phone: 215-955-6000<br>Appointment Phone: 1-800-JEFF-NOW (800-533-3669)",
+    	"url": "//hospitals.jefferson.edu/content/health/find-a-location/locations/jefferson-hospital-for-neuroscience-900-walnut-street.html",
+    	"position": { "lat": 39.9480344, "lng": -75.156207 }
+    },
+    {
+    	"type": "hospital",
+    	"region": "philadelphia",
+    	"name": "Methodist Hospital",
+    	"address": "2301 South Broad Street<br>Philadelphia, PA 19148",
+    	"phone": "Main Phone: 215-952-9000<br>Appointment Phone: 800-JEFF-NOW (800-533-3669)<br>Radiology Appointments: 215-952-1234<br>Outpatient Rehabilitation Services: 215-339-4280",
+    	"url": "//hospitals.jefferson.edu/content/health/find-a-location/locations/methodist-hospital.html",
+    	"position": { "lat": 39.921183, "lng": -75.169701 }
+    },
+    {
+      "type": "hospital",
+      "region": "bucks",
+      "name": "Jefferson Bucks Hospital",
+      "address": "380 North Oxford Valley Road<br>Langhorne, PA 19047",
+      "phone": "Phone: 215-949-5000",
+      "url": "https://www.ariahealth.org/bucks-county",
+      "position": { "lat": 40.1830928, "lng": -74.8725508 }
+    },
+    {
+      "type": "hospital",
+      "name": "Jefferson Frankford Hospital",
+      "region": "philadelphia",
+      "address": "4900 Frankford Avenue<br>Philadelphia, PA 19124",
+      "phone": "Phone: 215-831-2000",
+      "url": "https://www.ariahealth.org/frankford",
+      "position": { "lat": 40.0196895, "lng": -75.0838933 }
+    },
+    {
+      "type": "hospital",
+      "name": "Jefferson Torresdale Hospital",
+      "region": "philadelphia",
+      "address": "10800 Knights Road<br>Philadelphia, PA 19114",
+      "phone": "Phone: 215-612-4000",
+      "url": "https://www.ariahealth.org/torresdale",
+      "position": { "lat": 40.0716736, "lng": -74.9854092 }
+    },
+    {
+    	"type": "hospital",
+    	"region": "new jersey",
+    	"name": "Jefferson Cherry Hill Hospital",
+    	"address": "2201 Chapel Avenue West<br>Cherry Hill, NJ 08002",
+    	"phone": "Main phone: 856-488-6500",
+    	"url": "//hospitals.jefferson.edu/content/health/find-a-location/locations/jefferson-cherry-hill.html",
+    	"position": { "lat": 39.927982, "lng": -75.015943 }
+    },
+    {
+    	"type": "hospital",
+    	"region": "new jersey",
+    	"name": "Jefferson Stratford Hospital",
+    	"address": "2201 Chapel Avenue West<br>Cherry Hill, NJ 08002",
+    	"phone": "Main phone: 856-488-6500",
+    	"url": "//hospitals.jefferson.edu/content/health/find-a-location/locations/jefferson-stratford.html",
+    	"position": { "lat": 39.828275, "lng": -75.008195 }
+    },
+    {
+    	"type": "hospital",
+    	"region": "new jersey",
+    	"name": "Jefferson Washington Township Hospital",
+    	"address": "435 Hurffville-Cross Keys Road<br>Turnersville, NJ 08012",
+    	"phone": "Main phone: 856-582-2500",
+    	"url": "//hospitals.jefferson.edu/content/health/find-a-location/locations/jefferson-washington-township.html",
+    	"position": { "lat": 39.735239, "lng": -75.065340 }
+    },
+    {
+    	"type": "hospital",
+    	"region": "bucks",
+    	"name": "Rothman Orthopaedic Specialty Hospital &ndash; Bensalem",
+    	"address": "3300 Tillman Drive<br>Bensalem, PA 19020",
+    	"phone": "Phone: 1-800-JEFF-NOW",
+    	"url": "//hospitals.jefferson.edu/content/health/find-a-location/locations/rothman-orthopaedic-specialty-hospital.html",
+    	"position": { "lat": 40.1129362, "lng": -74.9627666 }
     }
   ];
 
@@ -62,6 +143,10 @@ function initMap() {
     });
   }
 
+  var locationMarker = new google.maps.Marker({
+    map: map
+  });
+
   /* Info Window */
 
   var infowindow = new google.maps.InfoWindow();
@@ -69,7 +154,16 @@ function initMap() {
   /* when marker is clicked, trigger info window */
   for (var i = 0; i < markers.length; i++) {
     markers[i].addListener('click', function() {
-      infowindow.setContent(this.location.name);
+      var location = locations[this.id];
+      var contentString =
+          '<div id="infoWindowContent">' +
+          '<div class="name">' + location.name + '</div>'+
+          '<div class="address">' + location.address + '</div>' +
+          '<div class="links">' +
+          '<a href="' + location.url + '">Locations Page</a>' + '<span> | </span>' +
+          '<a href="' + 'https://www.google.com/maps/place/' + location.address + '" target="blank">Directions</a>' +
+          '</div>';
+      infowindow.setContent(contentString);
       infowindow.open(map, this);
     });
   }
@@ -82,9 +176,13 @@ function initMap() {
 
     /* create locations list element, one for each marker */
     var $el = $('<li data-id="' + i + '">' +
-    '<h3 class="name">' + markers[i].location.name + '</h3>' +
-    '<p>' + markers[i].location.address + '</p>' +
-    '<p class="distance"></p>' +
+    '<div class="name">' + markers[i].location.name + '</div>' +
+    '<div class="address">' + markers[i].location.address + '</div>' +
+    '<div class="distance"></div>' +
+    '<div class="links">' +
+    '<a href="' + location.url + '">Locations Page</a>' + '<span> | </span>' +
+    '<a href="' + 'https://www.google.com/maps/place/' + location.address + '" target="blank">Directions</a>' +
+    '</div>' +
     '</li>');
     $listElement.append($el);
 
@@ -99,7 +197,13 @@ function initMap() {
 
     /* when map marker is clicked, trigger corresponding list item */
     markers[i].addListener('click', function() {
-      $('li[data-id="' + this.id + '"]').css('color', 'red');
+      var $listEl = $('li[data-id="' + this.id + '"]');
+      $listEl.addClass('is-active');
+      $('#locations-list li').not($listEl).removeClass('is-active');
+      $("body, html").animate({
+          scrollTop: $($listEl).offset().top
+      }, 600);
+
       console.log('li[data-id="' + this.id + '"]');
     });
   }
@@ -110,6 +214,11 @@ function initMap() {
   /* List Listener */
   locationsList.on('updated', function() {
     updateMarkers();
+    if (locationsList.matchingItems.length == locationsList.items.length) {
+      $('.list-message').hide()
+    } else {
+      $('.list-message').show()
+    }
   });
 
   function updateMarkers() {
@@ -155,9 +264,10 @@ function initMap() {
           var miles = distance_text.substring(0, distance_text.length - 3);
           console.log("Distance value is " + distance.value);
           console.log("Distance text is " + distance.text);
-          location.distance = distance.value / 5280;
+          location.distance = parseFloat(distance.value / 5280).toFixed(2);
           console.log("locations distance " + location.distance);
           $('li[data-id="' + location.id + '"]').attr("data-distance", location.distance);
+          $('li[data-id="' + location.id + '"]').find(".distance").html("Distance: " + location.distance + " mi");
           updateList();
           updateMarkers();
         }
@@ -198,8 +308,25 @@ function initMap() {
             var location = results[0].geometry.location,
                 lat      = location.lat(),
                 lng      = location.lng();
+          infowindow.close();
           map.setCenter(location);
-          map.setZoom(12);
+          var radius = $('#filterForm select option:selected').val();
+          if (radius === '1') {
+            map.setZoom(13);
+          } else if (radius === '5') {
+            map.setZoom(12);
+          } else if (radius === '10') {
+            map.setZoom(10);
+          } else if (radius === '25') {
+            map.setZoom(9);
+          } else if (radius === '50') {
+            map.setZoom(8);
+          } else if (radius === '100') {
+            map.setZoom(7);
+          } else {
+            map.setZoom(7);
+          }
+          locationMarker.setPosition(location);
         }
     });
   }
